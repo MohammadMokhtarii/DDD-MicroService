9594f7a5886af2a41c4b1651f9d947366589768b
Mohammad Mokhtari <Mohammad.Mokhtari86@gmail.com>
2023-12-17 10:22:58 +0330

    add application initialize and lazy patch to .net core built in di

---
+++ src/Services/NotificationManagement/NotificationManagement.Api/Core/ApplicationInitialize.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace NotificationManagement.Api.Core
+{
+    public class ApplicationInitialize : BackgroundService
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly ILogger<ApplicationInitialize> _logger;
+        public ApplicationInitialize(IServiceScopeFactory serviceScopeFactory)
+        {
+            ArgumentNullException.ThrowIfNull(nameof(serviceScopeFactory));
+
+            var serviceProvider = serviceScopeFactory.CreateScope();
+
+            _webHostEnvironment = serviceProvider.ServiceProvider.GetRequiredService<IWebHostEnvironment>();
+            _logger = serviceProvider.ServiceProvider.GetRequiredService<ILogger<ApplicationInitialize>>();
+
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            if (_webHostEnvironment.IsDevelopment())
+            {
+
+            }
+        }
+    }
+}
